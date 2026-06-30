f9b26485f8de9382a15216460a9af8e11d5b227f
Blessing1712 <blessingoluwayemisi2000@gmail.com>
2026-06-30 21:10:25 +0100

    Add funnel analysis: overall conversion rate

---
+++ SQL1/02_funnel_analysis.sql
@@ -0,0 +1,15 @@
+-- Overall conversion rate
+SELECT
+    COUNT(*) AS total_touchpoints,
+    SUM(Converted) AS total_conversions,
+    ROUND(
+        SUM(Converted) * 100.0 / COUNT(*), 2
+    ) AS overall_conversion_rate
+FROM fact_marketing;-- Overall conversion rate
+SELECT
+    COUNT(*) AS total_touchpoints,
+    SUM(Converted) AS total_conversions,
+    ROUND(
+        SUM(Converted) * 100.0 / COUNT(*), 2
+    ) AS overall_conversion_rate
+FROM fact_marketing;
